5d78fd2caa4691fd564993d31d71b99b9663ea35
platon517 <platon.517@gmail.com>
2020-10-31 02:30:38 +0300

    upd

--- routes/appUserRoutes.js
+++ routes/appUserRoutes.js
@@ -92,7 +92,7 @@ module.exports = app => {
       if (user.paymentMethods) {
         const paymentMethods =
           user.paymentMethods
-            .filter(method => method.entity.equals(entity))
+            .filter(method => (method.entity === entity))
             .map(method => ({ _id: method._id, card: method.card }));
         console.log(paymentMethods);
         return res.send(paymentMethods);
