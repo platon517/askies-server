ff93e44ea089dcb0e722ec992809848e9b513cb4
platon517 <platon.517@gmail.com>
2021-02-23 18:50:14 +0300

    port update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8080;
 const combinedRoutes = require('./routes/combinedRoutes');
 require('dotenv').config();
 
